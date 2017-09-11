e9b042a76d7d324472c9b3f5e1ab3bed2b489518
Islam <islam.shebl@vodafone.com>
2017-09-11 15:29:12 +0200

    last updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {EmployeeService} from './employees/service/employee.service'
 @Component({
   selector: 'vf-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[EmployeeService]
 })
 export class AppComponent {
   title = 'vf';
